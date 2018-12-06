72a978114e7345629d16558a07fee38605285900
Enrico Papi <enrico.papi@gmail.com>
2018-12-06 09:19:25 +0100

    fix parsing of process name

--- syslogchannel.cpp
+++ syslogchannel.cpp
@@ -6,10 +6,18 @@
 
 void SyslogChannelNew::log(const Poco::Message& msg)
 {
+
+    std::string msgtext = msg.getText();
+    const std::string::size_type i = msgtext.find(": ");
+    std::string msgproc = "NoProc";
+    if (i != std::string::npos)
+        msgproc = msgtext.substr(0, i);
+        msgtext = msgtext.substr(i + 2);
+
     const LogItem item {
         QString::fromStdString(Poco::DateTimeFormatter::format(msg.getTime(), "%m/%d %H:%M:%S")),
-        QString::fromStdString(msg.get("app", "NoProc")),
-        QString::fromStdString(msg.getText())
+        QString::fromStdString(msgproc),
+        QString::fromStdString(msgtext)
     };
 
     m_loglist->lockList();
